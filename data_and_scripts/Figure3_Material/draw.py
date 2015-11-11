3ea6f27a62bcb5de1a4581b591b6ef4f18214c2f
willch <williamchenthu@gmail.com>
2015-11-11 15:03:24 -0800

    update jags small batch figures

--- data_and_scripts/Figure3_Material/draw.py
+++ data_and_scripts/Figure3_Material/draw.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import csv
 
 
-with open('./50PercentFile/m1/kldiv', 'rb') as csvfile:
+with open('./50PercentFile/m1_long/kldiv', 'rb') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar=',')
     for row in spamreader:
         x = row
@@ -58,9 +58,7 @@ legend_labels = ["m = 1", "m = 5", "m = 10"]
 data = [kl_50_1, kl_50_5, kl_50_10]
 i = 0
 for i in xrange(3):
-    linewidth_num = 1.0
-    if i >= 3:
-        linewidth_num = 3.0
+    linewidth_num = 3
     plt.plot(data[i][:100], signals[i], label=legend_labels[i], linewidth=linewidth_num)
 
 # Be sure to increase the font sizes! I might also have to experiment with a lot of other settings.
@@ -90,10 +88,10 @@ timeInterval = (60 * 19 + 20.1 - time_25_10[0]) / 200
 for i in xrange(200):
     time_25_10[i] = time_25_10[0] + i * timeInterval
     
-time_50_1 = np.zeros(200)
+time_50_1 = np.zeros(2000)
 time_50_1[0] = 9.7
-timeInterval = (60 + 18.6 - time_50_1[0]) / 200
-for i in xrange(200):
+timeInterval = (60 * 11 + 52.6 - time_50_1[0]) / 200
+for i in xrange(2000):
     time_50_1[i] = time_50_1[0] + i * timeInterval
     
 time_50_5 = np.zeros(200)
@@ -121,7 +119,7 @@ data = [kl_50_1, kl_50_5, kl_50_10]
 time = [time_50_1, time_50_5, time_50_10]
 i = 0
 for i in xrange(3):
-    linewidth_num = 1.0
+    linewidth_num = 3
     
     plt.plot(time[i], data[i], label=legend_labels[i], linewidth=linewidth_num)
 
@@ -131,5 +129,5 @@ plt.title('Convergence Based on Seconds', fontsize='xx-large')
 plt.yscale('log')
 plt.xlabel('Seconds', fontsize='x-large')
 plt.ylabel('(Log)Average KL Divergence', fontsize='x-large')
-plt.xlim(0, 600)
+plt.xlim(0, 400)
 plt.savefig('fig_kl_div_25_50_perc_jags_time.png')

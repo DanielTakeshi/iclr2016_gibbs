f76a717eaee94b5d3e9ef20e77d00036a2dbc71d
willch <williamchenthu@gmail.com>
2015-11-17 14:36:37 -0800

    updated the performance

--- data_and_scripts/MoocSparsity/run2.sh
+++ data_and_scripts/MoocSparsity/run2.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 echo "run the 2.2% with updates"
-cd m1_2_2
+cd m1_2_2_mooc_sparse
 t2="$(time (jags runJagsExe))"
-echo "run the 2.5% with updates"
-cd ../m1_2_5
-t10="$(time (jags runJagsExe))"
 
 cd ../m1_5
 echo "run the 5% with updates"
+t30="$(time (jags runJagsExe))"
+
+cd ../m1_10
+echo "run the 10% with updates"
+t30="$(time (jags runJagsExe))"
+
+cd ../m1_25
+echo "run the 25% with updates"
 t30="$(time (jags runJagsExe))"
 
 cd ..

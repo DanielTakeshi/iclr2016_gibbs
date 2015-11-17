d088467bc6e62b5a32da0700a6d09e018bc6891e
Daniel Seita <takeshidanny@gmail.com>
2015-11-17 14:49:31 -0800

    Cleaning up directory, adding an image

--- data_and_scripts/Figure4_Material/draw_fig.py
+++ data_and_scripts/Figure4_Material/draw_fig.py
@@ -1,61 +1,43 @@
 """
 (c) 2015 by Daniel Seita
 
-This will create the plots for Figure 4, showing the KL-time tradeoff for the 5-variable data.
-
-To be honest, I am not sure if this is what we want to do...
-
-Batch size: 25,000. Total data: 50,000.
+Test for accuracy with our BayesNet.scala on different levels of sparsity for synthetic mooc data.
 """
 
 import matplotlib.pyplot as plt
 import numpy as np
 
-# These contain the BIDMach outout that gets printed. First, delete everything but the raw data
-# itself. Also, that raw data needs to have the AVERAGE of the KL divergences across *all*
-# mini-batches, not just the ones that are "testing" (b/c no notion of testing here, really).
 output_files = [
-     "kl_time_tradeoff_koller_01same_1337seed.txt",
-     "kl_time_tradeoff_koller_05same_1337seed.txt",
-     "kl_time_tradeoff_koller_10same_1337seed.txt"
+    "kldiv_synthmooc022_matchsparsity_02b_500iter.txt",
+    "kldiv_synthmooc050_02b_500iter.txt",
+    "kldiv_synthmooc100_02b_500iter.txt",
+    "kldiv_synthmooc250_02b_500iter.txt"
 ]
 
-kldiv = []
-times = []
+factor = 2 ## NOTE NOTE NOTE Change as needed!
 
-for (i,element) in enumerate(output_files):
-    with open(element, 'r') as log_file:
+data = []
+for (i,item) in enumerate(output_files):
+    with open(item, 'r') as log_file:
         kls = []
-        tms = []
         for line in log_file:
             line_split = line.split()
             if (len(line_split) >= 3 and line_split[2] == "KLDiv"):
                 kls.append(float(line_split[0]))
-            elif (len(line_split) >= 4 and ("secs" in line_split[3])):
-                further_split = line_split[3].split("=")
-                tms.append(float(further_split[1].rstrip(",")))
-        kldiv.append( [(a+b)/2.0 for (a,b) in zip(kls[::2], kls[1::2])] ) # Avg every two elems
-        times.append(tms)
-assert len(kldiv) == len(output_files)
+        data.append(kls[factor::factor]) 
 
 # Now plot!
-signals = ['r-', 'y-', 'b-', 'c*-', 'm*-']
-legend_labels = [
-    "m = 1",
-    "m = 5",
-    "m = 10"
-]
+signals = ['r-', 'y-', 'b-', 'k-']
+legend_labels = ["2.2% known", "5% known", "10% known", "25% known"]
 
 plt.figure()
 for i in range(len(output_files)):
-    plt.plot(times[i], kldiv[i], signals[i], label=legend_labels[i], linewidth=2.0)
+    plt.plot(data[i][:500], signals[i], label=legend_labels[i], linewidth=3.0)
 
 # Be sure to increase the font sizes! I might also have to experiment with a lot of other settings.
 plt.legend(loc='upper right', ncol=1)
-plt.title('Average KL and Time Tradeoff (Koller)', fontsize='xx-large')
-plt.xlabel('Time (Seconds)', fontsize='x-large')
-plt.xlim([0,10])
+plt.title('BIDMach Performance (Synthetic MOOC)', fontsize='xx-large')
+plt.xlabel('Number of Passes Over the Data', fontsize='x-large')
 plt.ylabel('Average KL Divergence', fontsize='x-large')
 plt.yscale('log')
-plt.ylim([10**(-5),10**(-1)])
-plt.savefig('kl_time_tradeoff_koller_data.png')
+plt.savefig('test.png')
